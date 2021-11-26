e41d4380c29596170f448e3d223e3ae26944b369
akinhet <piti200406@gmail.com>
2021-11-27 00:46:27 +0100

    Zadanie 1

---
+++ cipher.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <string>
+
+using std::string, std::cout, std::cin, std::endl;
+
+
+string caesar(int offset, string buffer)
+{
+	if (!buffer.length())
+		return "";
+	if (!(offset > 0 && offset < 26)) {
+		cout << "ERROR: Offset outside of range (1-25)\n";
+		exit(2);
+	}
+
+	for (int i=0; i <= buffer.length(); i++)
+		buffer[i] = (buffer[i] - 97 + offset) % 26 + 97;
+
+	return buffer;
+}
+
+
+string shift(string buffer)
+{
+	if (!buffer.length())
+		return "";
+
+	char temp;
+
+	for (int i=0; i <= (int)(buffer.length() / 2); i+=2) {
+		temp = buffer[i];
+		buffer[i] = buffer[i+1];
+		buffer[i+1] = temp;
+	}
+
+	return buffer;
+}
+
+
+void decrypt(string buffer)
+{
+	for (int offset=0; offset <= 26; offset++) {
+		string temp = buffer;
+
+		for (int j=0; j <= temp.length(); j++)
+			temp[j] = (26 + temp[j] - 97 - offset) % 26 + 97;
+		cout << offset << ": " << temp << endl;
+	}
+}
+
+
+int main()
+{
+	string buffer;
+	int choice, temp;
+
+	cout << "Write what you want to cipher (only lowercase letters): ";
+	getline(cin, buffer);
+	
+	cout << "1. Caesar cipher\n2. Shifting cipher\n3. Caesar and shifting cipher\n4. Decipher message\n";
+	cin >> choice;
+
+	switch (choice) {
+		case 1:
+			cout << "Rotation (1-25): ";
+			cin >> temp;
+			cout << caesar(temp, buffer) << endl;
+			break;
+		case 2:
+			cout << shift(buffer) << endl;
+			break;
+		case 3:
+			cout << "Rotation (1-26): ";
+			cin >> temp;
+			cout << shift(caesar(temp, buffer)) << endl;
+			break;
+		case 4:
+			decrypt(buffer);
+			break;
+		default:
+			cout << "ERROR: Unknown option\n";
+			exit(1);
+	}
+
+	return 0;
+}
