60015d5bdc65654ca0d9cff63562a74982597132
akinhet <piti200406@gmail.com>
2021-11-27 11:27:32 +0100

    Added classes Cypher and Decypher

--- cipher.cpp
+++ cipher.cpp
@@ -4,53 +4,60 @@
 using std::string, std::cout, std::cin, std::endl;
 
 
-string caesar(int offset, string buffer)
-{
-	if (!buffer.length())
-		return "";
-	if (!(offset > 0 && offset < 26)) {
-		cout << "ERROR: Offset outside of range (1-25)\n";
-		exit(2);
-	}
+class Cypher {
+	public:
+		string caesar(int offset, string buffer)
+		{
+			if (!buffer.length())
+				return "";
+			if (!(offset > 0 && offset < 26)) {
+				cout << "ERROR: Offset outside of range (1-25)\n";
+				exit(2);
+			}
 
-	for (int i=0; i <= buffer.length(); i++)
-		buffer[i] = (buffer[i] - 97 + offset) % 26 + 97;
+			for (int i=0; i <= buffer.length(); i++)
+				buffer[i] = (buffer[i] - 97 + offset) % 26 + 97;
 
-	return buffer;
-}
+			return buffer;
+		}
+
+		string shift(string buffer)
+		{
+			if (!buffer.length())
+				return "";
+
+			char temp;
+
+			for (int i=0; i <= (int)(buffer.length() / 2); i+=2) {
+				temp = buffer[i];
+				buffer[i] = buffer[i+1];
+				buffer[i+1] = temp;
+			}
+
+			return buffer;
+		}
+};
 
 
-string shift(string buffer)
-{
-	if (!buffer.length())
-		return "";
+class Decypher {
+	public:
+		void decrypt(string buffer)
+		{
+			for (int offset=0; offset <= 26; offset++) {
+				string temp = buffer;
 
-	char temp;
-
-	for (int i=0; i <= (int)(buffer.length() / 2); i+=2) {
-		temp = buffer[i];
-		buffer[i] = buffer[i+1];
-		buffer[i+1] = temp;
-	}
-
-	return buffer;
-}
-
-
-void decrypt(string buffer)
-{
-	for (int offset=0; offset <= 26; offset++) {
-		string temp = buffer;
-
-		for (int j=0; j <= temp.length(); j++)
-			temp[j] = (26 + temp[j] - 97 - offset) % 26 + 97;
-		cout << offset << ": " << temp << endl;
-	}
-}
+				for (int j=0; j <= temp.length(); j++)
+					temp[j] = (26 + temp[j] - 97 - offset) % 26 + 97;
+				cout << offset << ": " << temp << endl;
+			}
+		}
+};
 
 
 int main()
 {
+	Cypher c = Cypher();
+	Decypher d = Decypher();
 	string buffer;
 	int choice, temp;
 
@@ -64,18 +71,18 @@ int main()
 		case 1:
 			cout << "Rotation (1-25): ";
 			cin >> temp;
-			cout << caesar(temp, buffer) << endl;
+			cout << c.caesar(temp, buffer) << endl;
 			break;
 		case 2:
-			cout << shift(buffer) << endl;
+			cout << c.shift(buffer) << endl;
 			break;
 		case 3:
 			cout << "Rotation (1-26): ";
 			cin >> temp;
-			cout << shift(caesar(temp, buffer)) << endl;
+			cout << c.shift(c.caesar(temp, buffer)) << endl;
 			break;
 		case 4:
-			decrypt(buffer);
+			d.decrypt(buffer);
 			break;
 		default:
 			cout << "ERROR: Unknown option\n";
