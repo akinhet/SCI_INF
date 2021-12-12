3374b96fe110b4b711c18632e9accf034bb92455
akinhet <piti200406@gmail.com>
2021-12-12 23:07:19 +0100

    Zadanie 3

--- rsakeys.cpp
+++ rsakeys.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <utility>
+#include <algorithm>
+#include <iterator>
 #include <time.h>
 #include <stdlib.h>
 
@@ -28,6 +30,19 @@ int gcd(int a, int b)
         a = b;
         b = temp;
     }
+}
+
+
+long long powmod(long long t, long long e, long long n)
+{
+    long long r = 1;
+
+    for (e; e > 0; e /= 2) {
+        if (e % 2)
+            r = (r * t) % n;
+        t = (t * t) % n;
+    }
+    return r;
 }
 
 
@@ -60,10 +75,62 @@ std::pair<struct publickey, struct privatekey> genrsakeys()
 }
 
 
+void rsaencrypt(std::string message, long long e, long  long n)
+{
+    for (size_t i = 0; i < message.size(); i++) {
+        std::cout << powmod(message[i], e, n) << " ";
+    }
+    std::cout << std::endl;
+}
+
+
+void rsadecrypt(long long c[], size_t size, long long d, long long n)
+{
+    for (size_t i = 0; i < size; i++) {
+        std::cout << (char)powmod(c[i], d, n);
+    }
+    std::cout << std::endl;
+}
+
+
 int main()
 {
-    std::pair<struct publickey, struct privatekey>keys = genrsakeys();
-    std::cout << "Private key: n=" << keys.second.n << " d=" << keys.second.d << "\nPublic key: n=" << keys.first.n << " e=" << keys.first.e << std::endl;
+    int temp;
+    std::string message;
+    long long e, n;
+    long long *c;
+    size_t count;
+    std::cout << "1. Generate RSA keys\n2. Encrypt a message using RSA keys\n3. Decrypt a number using RSA keys\n";
+    std::cin >> temp;
+    switch (temp) {
+        case 1: {
+            std::pair<struct publickey, struct privatekey>keys = genrsakeys();
+            std::cout << "Private key: n=" << keys.second.n << " d=" << keys.second.d << "\nPublic key: n=" << keys.first.n << " e=" << keys.first.e << std::endl;
+            break; }
+        case 2:
+            std::cout << "Message you want to encrypt: ";
+            std::cin >> message;
+            std::cout << "E: ";
+            std::cin >> e;
+            std::cout << "N: ";
+            std::cin >> n;
+            rsaencrypt(message, e, n);
+            break;
+        case 3:
+            std::cout << "How long is the message? ";
+            std::cin >> count;
+            c = new long long[count];
+            std::cout << "Message you want to decrypt: ";
+            std::copy_n(std::istream_iterator<long long>(std::cin), count, c);
+            std::cout << "D: ";
+            std::cin >> e;
+            std::cout << "N: ";
+            std::cin >> n;
+            rsadecrypt(c, count, e, n);
+            break;
+        default:
+            break;
+    }
 
     return 0;
 }
